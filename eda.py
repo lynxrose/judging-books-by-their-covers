99f47238017615eaf78f19b588b2f9aedd1118ab
Lynx Rose <lynx@dropinmedia.com>
2020-12-04 16:26:04 -0800

    after presentaion

--- eda.py
+++ eda.py
@@ -1,25 +1,44 @@
+#imports
 import pandas as pd
 import matplotlib.pyplot as plt
+train = pd.read_csv('charts/train.csv')
 
-best_df = pd.read_csv('charts/best_sellers.csv')
-non_df = pd.read_csv('charts/non_best_sellers.csv')
-
+#Graph Top Published Authors
 fig = plt.figure()
-most_books_per_author = best_df.groupby(by = 'author').size().nlargest(15)
+most_books_per_author = train.groupby(by = 'author').size().nlargest(15)
 plt.xticks(rotation=90)
 plt.ylabel('Number of Books')
 plt.ticklabel_format(style = 'plain')
 ax = plt.bar(most_books_per_author.index,most_books_per_author.values)
-plt.title('Top Bestseller Authors')
-plt.savefig('graphs/Top_Bestseller_Authors.png',bbox_inches = 'tight')
+plt.title('Top Published Authors')
+plt.savefig('graphs/Top_Published_Authors.png',bbox_inches = 'tight')
+plt.show()
 
-epoch_scores = [0.7580, 0.7487, 0.7446, 0.7549, 0.7611, 0.7621, 0.7405, 0.7693, 0.7662, 0.7652, 0.7621,  0.7621, 0.7714, 0.7477, 0.7631, 0.7621, 0.7611, 0.7683, 0.7446, 0.7662]
-#current at 15 include 16
-fig, ax = plt.subplots()
-ax.set_xticks(list(range(0,22))[0:21])
-ax.set_xticklabels(list(range(0,22))[0:21])
-ax.set_title('accuracy as epochs increase')
-ax.set_ylabel('test accuracy')
-ax.set_xlabel('epochs')
-ax.plot(list(range(len(epoch_scores))),epoch_scores)
+#Per Epoch scores
+val_accuracy = [0.7580, 0.7487, 0.7446, 0.7549, 0.7611, 0.7621, 0.7405, 0.7693, 0.7662, 0.7652, 0.7621,  0.7621, 0.7714, 0.7477, 0.7631, 0.7621, 0.7611, 0.7683, 0.7446, 0.7662]
+training_loss = [0.2458, 0.1392, 0.0944, 0.0662, 0.0590, 0.0465, 0.0287, 0.0319, 0.0242, 0.0346, 0.0378, 0.0417, 0.0147, 0.0160, 0.0202, 0.0207, 0.0237, 0.0089, 0.0153, 0.0181]
+val_loss = [0.6095, 0.6884, 1.0347, 0.9148, 0.9752, 1.1152, 1.3549, 1.2024, 1.2577, 1.1695, 1.1316, 0.9850, 1.2032, 1.2496, 1.1746, 1.1113, 1.0719, 1.4005, 1.1661, 1.1311]
+train_accuracy = [0.8990, 0.9511, 0.9717, 0.9779, 0.9840, 0.9871, 0.9912, 0.9920, 0.9959, 0.9887, 0.9897,0.9897, 0.9956, 0.9954, 0.9946, 0.9969, 0.9951, 0.9969, 0.9959, 0.9951]
+
+#Accuracy and Loss as Epochs Increase Graphs
+fig, ax = plt.subplots(2)
+ax[0].set_xticks(list(range(0,22))[0:21])
+ax[0].set_xticklabels(list(range(1,22))[0:21])
+ax[0].set_title('Accuracy and Loss as Epochs Increase')
+ax[0].set_ylabel('Accuracy')
+ax[0].set_xlabel('Number of Epochs')
+
+ax[1].set_xticks(list(range(0,22))[0:21])
+ax[1].set_xticklabels(list(range(1,22))[0:21])
+ax[1].set_ylabel('Loss')
+ax[1].set_xlabel('Number of Epochs')
+
+ax[0].plot(list(range(len(val_accuracy))),val_accuracy, label = 'val_accuracy', color = 'b', marker='.')
+ax[0].plot(list(range(len(train_accuracy))),train_accuracy, label = 'train_accuracy', color = 'r', marker='.')
+
+ax[1].plot(list(range(len(val_loss))),val_loss, label = 'val_loss', color = 'b', marker='.')
+ax[1].plot(list(range(len(training_loss))),training_loss, label = 'train_loss', color = 'r', marker='.')
+ax[1].legend()
+ax[0].legend()
+plt.show()
 plt.savefig('graphs/epoch_scores.png', bbox_inches = 'tight')
--- modeling.py
+++ modeling.py
@@ -1,3 +1,4 @@
+#imports
 from tensorflow import keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Flatten, LSTM
@@ -7,27 +8,17 @@ from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.vgg16 import preprocess_input
 from tensorflow.keras.preprocessing.image import load_img
 from tensorflow.keras.models import load_model
-from PIL import Image as Img
-from PIL import ImageTk
-
-from tensorflow.keras import preprocessing
-from tensorflow.keras import backend as K
-from tensorflow.keras import models
-
 import tensorflow as tf
-
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.utils import to_categorical
-from tqdm import tqdm
-
 train = pd.read_csv('charts/train.csv')
 
+#Collect X images and standardize
 train_image = []
 best_list = []
-for i in tqdm(range(len(train))):
+for i in range(len(train)):
     if list(train.best)[i] == 1:
         folder = 'best_sellers_images'
         best_list.append(1)
@@ -36,6 +27,7 @@ for i in tqdm(range(len(train))):
         best_list.append(0)
     img = image.load_img(folder+'/'+list(train.image)[i], target_size=(128,128,3), grayscale=False)
     img = image.img_to_array(img)
+    #standardize
     img = img/255
     train_image.append(img)
 X = np.array(train_image)
@@ -44,6 +36,7 @@ y = to_categorical(y_)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
+#Building CNN for RGB
 model = Sequential()
 model.add(Conv2D(32, kernel_size=(3, 3),activation='relu',input_shape=(128,128,3)))
 model.add(Conv2D(64, (3, 3), activation='relu'))
@@ -56,12 +49,24 @@ model.add(Dense(2, activation='softmax'))
 
 model.compile(loss='categorical_crossentropy',optimizer='Adam',metrics=['accuracy'])
 
-model.fit(X_train, y_train, epochs=20, validation_data=(X_test, y_test))
+#train
+model.fit(X_train, y_train, epochs=4, validation_data=(X_test, y_test))
 
-model.save('models/cover_image.h5')
+#save the model
+model.save('cover_image.h5')
 
-def singleImgTest(target):
+def singleImgTest(image_path):
     img = image.load_img(target, target_size=(128,128,3), grayscale=False)
     img = image.img_to_array(img)
     img = img/255
     return model.predict(np.array([img]))
+
+#Score
+scoreList = model.predict(X)
+goodScore = []
+for score in scoreList:
+    goodScore.append(score[1])
+train['score'] = goodScore
+
+confusion = tf.math.confusion_matrix(labels=list(train.best), predictions=list(round(train['score'])), num_classes=2)
+seaborn.heatmap(confusion.numpy(), annot=True, fmt="d")
